698482809eb7362042ff4fe4bda9f3e7b0c9ffa4
Fabrício Yamamoto <fabriciorby@hotmail.com>
2019-02-17 21:29:13 -0300

    1043 refatorado

--- Beginner/1043.js
+++ Beginner/1043.js
@@ -7,15 +7,12 @@ try {
 
 const line = lines.shift().split(' ').map(item => parseFloat(item));
 
-let isTriangulo = (listaDePontos) => {
-    let somaDosPontos = listaDePontos.reduce((soma, n) => soma + n);
-    return listaDePontos.every((element, index) => {
-        let subtracaoDosPontos = index === 0 ? somaDosPontos - element - 2*listaDePontos[2] : somaDosPontos - element - 2*listaDePontos[0];
-        return (somaDosPontos - element > element && Math.abs(subtracaoDosPontos) < element);
-    });
-};
+line.sort((a, b) => b - a);
+const a = line[0];
+const b = line[1];
+const c = line[2];
 
-if (isTriangulo(line)) {
+if (a < b + c) {
     console.log('Perimetro = ' + line.reduce((soma, n) => soma + n).toFixed(1));
 } else {
     console.log('Area = ' + ((line[0] + line[1]) * line[2] / 2).toFixed(1));
